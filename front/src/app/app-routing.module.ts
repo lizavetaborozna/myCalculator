a42ee5a2b82873649c082ddfcb532a980c48787e
Elizaveta Borozna <lizavetaborozna@gmail.com>
2019-08-16 13:27:11 +0300

    change routes

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,21 +1,20 @@
-import  { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ResultListComponent } from './result-list/result-list.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ResultListComponent} from './result-list/result-list.component';
 import {CalculatorComponent} from "./calculator/calculator.component";
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/calculator', pathMatch: 'full' },
-  { path: 'results', component: ResultListComponent },
-  { path: 'calculator', component: CalculatorComponent },
-  { path: 'tax', component: CalculatorComponent },
-  { path: 'filter', component: ResultListComponent },
-  { path: 'filter1', component: ResultListComponent },
-  { path: 'filter2', component: ResultListComponent },
+  {path: '', redirectTo: '/calculator', pathMatch: 'full'},
+  {path: 'results', component: ResultListComponent},
+  {path: 'calculator', component: CalculatorComponent},
+  {path: 'tax', component: CalculatorComponent},
+  {path: 'filter', component: ResultListComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
